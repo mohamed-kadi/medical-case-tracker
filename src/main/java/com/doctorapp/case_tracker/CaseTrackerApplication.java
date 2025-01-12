dd47d46b019a2e85d7db40285d490e2da2cd42f0
mohamed.kadi <n.kadi63@gmail.com>
2025-01-12 18:04:35 -0500

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/doctorapp/case_tracker/CaseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.doctorapp.case_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctorapp/case_tracker/CaseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorapp.case_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
